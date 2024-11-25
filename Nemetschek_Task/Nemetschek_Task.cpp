38108b4fcf1ce2cd2dd19430c53e008abc45014c
Adaptl <albedahTr07@protonmail.com>
2024-11-25 09:34:43 +0200

    Basic error handling in main.

--- Nemetschek_Task/Nemetschek_Task.cpp
+++ Nemetschek_Task/Nemetschek_Task.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
+#include <cstdlib>
 #include "Application.h"
+
+Application* InitializeApplication();
+
+void DeleteApplication(Application* app);
 
 int main()
 {
-    Application app;
-    app.Run();
+    Application* app = InitializeApplication();
 
-    return 0;
+    if (!app)
+        return EXIT_FAILURE;
+
+    try
+    {
+        app->Run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Exception occurred: " << e.what() << std::endl;
+        DeleteApplication(app);
+        return EXIT_FAILURE;
+    }
+    catch (...)
+    {
+        std::cerr << "Failed to run application!" << std::endl;
+        DeleteApplication(app);
+        return EXIT_FAILURE;
+    }
+
+    DeleteApplication(app);
+
+    return EXIT_SUCCESS;
+}
+
+Application* InitializeApplication()
+{
+    return new Application();
+}
+
+void DeleteApplication(Application* app)
+{
+    if (app != nullptr)
+        delete app;
 }
