edd2c0f3e49f0e359a0b7bf91d7c7738d26f0277
Adaptl <albedahTr07@protonmail.com>
2024-11-25 10:26:47 +0200

    Dimension class implementation.

---
+++ Nemetschek_Task/Dimension.cpp
@@ -0,0 +1,188 @@
+#include "Dimension.h"
+#include <stdexcept>
+#include <sstream>
+#include <numeric>
+
+Dimension::Dimension(unsigned width, unsigned height)
+    : Dimension(width, height, Units::MILLIMETER) {}
+
+Dimension::Dimension(unsigned width, unsigned height, Units unitType)
+{
+    SetWidth(width, unitType);
+    SetHeight(height, unitType);
+    this->m_currentUnit = unitType;
+}
+
+Dimension::Dimension(const AspectRatio& ratio)
+    : Dimension(0,0,Units::MILLIMETER)
+{
+    this->m_ratio = new AspectRatio(ratio);
+    if (!this->m_ratio)
+        throw std::runtime_error("Failed to initialize aspect ratio!");
+}
+
+unsigned Dimension::GetWidth() const
+{
+    if (m_ratio)
+        return m_ratio->width;
+
+    return ConvertToUnit(m_width, m_currentUnit);
+}
+
+unsigned Dimension::GetHeight() const
+{
+    if (m_ratio)
+        return m_ratio->height;
+
+    return ConvertToUnit(m_width, m_currentUnit);
+}
+
+unsigned Dimension::GetWidth(Units unit) const
+{
+    if (m_ratio)
+        return m_ratio->width;
+
+    return ConvertToUnit(m_width, unit);
+}
+
+unsigned Dimension::GetHeight(Units unit) const
+{
+    if (m_ratio)
+        return m_ratio->height;
+
+    return ConvertToUnit(m_height, unit);
+}
+
+Dimension::Units Dimension::GetUnits() const
+{
+    return m_currentUnit;
+}
+
+unsigned Dimension::ConvertWidthToAspectRatio() const
+{
+    unsigned gcd = Dimension::GCD(m_width, m_height);
+    return m_width / gcd;
+}
+
+unsigned Dimension::ConvertHeightToAspectRatio() const
+{
+    unsigned gcd = Dimension::GCD(m_width, m_height);
+    return m_height / gcd;
+}
+
+void Dimension::SetWidth(unsigned width, Units unit = Units::MILLIMETER)
+{
+    if (m_ratio)
+    {
+        m_ratio->width = width;
+        return;
+    }
+
+    m_width = ConvertToMillimetres(width, unit);
+}
+
+void Dimension::SetHeight(unsigned height, Units unit = Units::MILLIMETER)
+{
+    if (m_ratio)
+    {
+        m_ratio->height = height;
+        return;
+    }
+    m_height = ConvertToMillimetres(height, unit);
+}
+
+void Dimension::SetUnits(Units unit)
+{
+    m_currentUnit = unit;
+}
+
+void Dimension::SetAspectRatio(const AspectRatio& ratio)
+{
+    if (!this->m_ratio)
+    {
+        this->m_ratio = new AspectRatio(ratio);
+        if (!this->m_ratio)
+            throw std::runtime_error("Failed to initialize aspect ratio!");
+        return;
+    }
+
+    this->m_ratio->width  = ratio.width;
+    this->m_ratio->height = ratio.height;
+}
+
+std::string Dimension::ToString() const
+{
+    std::ostringstream result;
+    std::string unitStr = "";
+
+    switch (m_currentUnit)
+    {
+    case Units::MILLIMETER:
+        unitStr = MILLIMETER_STR;
+        break;
+    case Units::CENTIMETER:
+        unitStr = CENTIMETER_STR;
+        break;
+    case Units::METER:
+        unitStr = METER_STR;
+        break;
+    default:
+        throw std::runtime_error("Unknown unit type");
+    }
+
+    std::string seperationStr = (m_ratio == nullptr ? " x " : ":");
+
+    result << "Dimensions: "
+        << GetWidth(m_currentUnit)  << unitStr << seperationStr
+        << GetHeight(m_currentUnit) << unitStr;
+
+    return result.str();
+}
+
+unsigned Dimension::ConvertToUnit(unsigned value, Units unit) const
+{
+    switch (unit)
+    {
+    case Units::MILLIMETER:
+        return value;
+    case Units::CENTIMETER:
+        return value / 10;
+    case Units::METER:
+        return value / 1000;
+    default:
+        throw std::runtime_error("Unknown unit type");
+    }
+}
+
+unsigned Dimension::ConvertToMillimetres(unsigned value, Units unit) const
+{
+    switch (unit)
+    {
+    case Units::MILLIMETER:
+        return value;
+    case Units::CENTIMETER:
+        return value * 10;
+    case Units::METER:
+        return value * 1000;
+    default:
+        throw std::runtime_error("Unknown unit type");
+    }
+}
+
+unsigned Dimension::GCD(unsigned a, unsigned b) const
+{
+    while (b != 0)
+    {
+        unsigned temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+
+Dimension::~Dimension()
+{
+    if (m_ratio != nullptr)
+        delete m_ratio;
+}
---
+++ Nemetschek_Task/Dimension.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "IDataObject.h"
+
+#define MILLIMETER_STR "mm"
+#define CENTIMETER_STR "cm"
+#define METER_STR      "m"
+
+class Dimension : public IDataObject
+{
+public:
+	enum class Units { MILLIMETER, CENTIMETER, METER };
+
+	struct AspectRatio
+	{
+		AspectRatio(unsigned w, unsigned h) : width(w), height(h) {}
+		unsigned width;
+		unsigned height;
+	};
+	
+	Dimension(unsigned width, unsigned height);
+	Dimension(unsigned width, unsigned height, Units unitType);
+	Dimension(const AspectRatio& ratio);
+
+	unsigned GetWidth()            const;
+	unsigned GetHeight()           const;
+	unsigned GetWidth(Units unit)  const;
+	unsigned GetHeight(Units unit) const;
+	Units    GetUnits()            const;
+
+	unsigned ConvertWidthToAspectRatio()  const;
+	unsigned ConvertHeightToAspectRatio() const;
+
+	void SetWidth(unsigned width,   Units unit = Units::MILLIMETER);
+	void SetHeight(unsigned height, Units unit = Units::MILLIMETER);
+	void SetUnits(Units unit);
+	void SetAspectRatio(const AspectRatio& ratio);
+
+	std::string ToString() const override;
+
+	~Dimension();
+private:
+	unsigned ConvertToMillimetres(unsigned value, Units unit) const;
+	unsigned ConvertToUnit(unsigned value, Units unit)        const;
+
+	unsigned GCD(unsigned a, unsigned b) const;
+
+	unsigned   m_width;
+	unsigned   m_height;
+	Units      m_currentUnit;
+
+	AspectRatio* m_ratio;
+};
+
