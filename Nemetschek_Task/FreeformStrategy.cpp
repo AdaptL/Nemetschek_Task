d5e29b04cd2841a99f925cc59eb79ed0603882a1
Adaptl <albedahTr07@protonmail.com>
2024-11-27 23:01:36 +0200

    Guard against zero division in freeform strategy.

--- Nemetschek_Task/FreeformStrategy.cpp
+++ Nemetschek_Task/FreeformStrategy.cpp
@@ -1,9 +1,13 @@
+#include <stdexcept>
 #include "FreeformStrategy.h"
 
 Dimension FreeformStrategy::GenerateWallDimension(const Dimension& panelSize, 
                                                   const Dimension& maxSize) const
 {
     Dimension result;
+
+    if (panelSize.GetWidth() == 0 || panelSize.GetHeight() == 0)
+        throw std::invalid_argument("Panel width or height cannot be zero!");
 
     unsigned cols = maxSize.GetWidth() / panelSize.GetWidth();
     unsigned rows = maxSize.GetHeight() / panelSize.GetHeight();
